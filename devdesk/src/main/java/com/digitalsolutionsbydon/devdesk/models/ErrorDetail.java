3637f8e28765aea04f1918494a196bd31fa26064
Donald Whitely <dswhitely1@gmail.com>
2019-08-25 14:20:54 -0400

    Added API Documentation for ErrorDetail

--- devdesk/src/main/java/com/digitalsolutionsbydon/devdesk/models/ErrorDetail.java
+++ devdesk/src/main/java/com/digitalsolutionsbydon/devdesk/models/ErrorDetail.java
@@ -1,6 +1,8 @@
 package com.digitalsolutionsbydon.devdesk.models;
 
 import com.digitalsolutionsbydon.devdesk.exceptions.ValidationError;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -8,13 +10,39 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@ApiModel(value = "ErrorDetail",
+        description = "Error Messages")
 public class ErrorDetail
 {
+    @ApiModelProperty(name = "title",
+            value = "Error Title",
+            required = false,
+            example = "Resource Not Found")
     private String title;
+    @ApiModelProperty(name = "status",
+            value = "Error Status",
+            required = false,
+            example = "404")
     private int status;
+    @ApiModelProperty(name = "detail",
+            value = "Error details",
+            required = false,
+            example = "User 400 is not in the system.")
     private String detail;
+    @ApiModelProperty(name = "timestamp",
+            value = "Time Error Occurred",
+            required = false,
+            example = "25 Aug 2019 12:46:38:887 -0400")
     private String timestamp;
+    @ApiModelProperty(name = "developerMessage",
+            value = "Developer Specific Messsage",
+            required = false,
+            example = "com.digitalsolutionsbydon.devdesk.exceptions.ResourceNotFoundException")
     private String developerMessage;
+    @ApiModelProperty(name = "Validation Errors",
+            value = "Validation Errors",
+            required = false,
+            example = "{}")
     private Map<String, List<ValidationError>> errors = new HashMap<>();
 
     public String getTitle()
