b54ea8bfcc1d3b0fb2f062d503a88bd34af2b41c
Donald Whitely <dswhitely1@gmail.com>
2019-08-25 09:28:44 -0400

    UserAuditing Service defined

---
+++ devdesk/src/main/java/com/digitalsolutionsbydon/devdesk/services/UserAuditing.java
@@ -0,0 +1,27 @@
+package com.digitalsolutionsbydon.devdesk.services;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class UserAuditing implements AuditorAware<String>
+{
+    @Override
+    public Optional<String> getCurrentAuditor()
+    {
+        String uname;
+        Authentication authentication = SecurityContextHolder.getContext()
+                                                             .getAuthentication();
+        if (authentication != null)
+        {
+            uname = authentication.getName();
+        } else
+        {
+            uname = "SYSTEM";
+        }
+
+        return Optional.of(uname);
+    }
+}
