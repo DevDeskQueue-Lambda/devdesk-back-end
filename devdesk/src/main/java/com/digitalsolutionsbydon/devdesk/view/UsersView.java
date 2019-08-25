674cb58385131a28a1e599f7e1195c8ea55096f1
Donald Whitely <dswhitely1@gmail.com>
2019-08-25 17:49:38 -0400

    Testing UserView Response

---
+++ devdesk/src/main/java/com/digitalsolutionsbydon/devdesk/view/UsersView.java
@@ -0,0 +1,13 @@
+package com.digitalsolutionsbydon.devdesk.view;
+
+import java.util.List;
+
+public interface UsersView
+{
+    int getUserid();
+    String getUsername();
+    String getFname();
+    String getLname();
+    String getUseremail();
+    List<String> getName();
+}
