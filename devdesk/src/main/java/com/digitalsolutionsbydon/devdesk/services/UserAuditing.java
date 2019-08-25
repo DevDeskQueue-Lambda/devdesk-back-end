a63b920e680103c152fc5ca3186cfeffb924f37c
Donald Whitely <dswhitely1@gmail.com>
2019-08-25 14:20:35 -0400

    Added @Component for User Auditing

--- devdesk/src/main/java/com/digitalsolutionsbydon/devdesk/services/UserAuditing.java
+++ devdesk/src/main/java/com/digitalsolutionsbydon/devdesk/services/UserAuditing.java
@@ -3,9 +3,11 @@ package com.digitalsolutionsbydon.devdesk.services;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component
 public class UserAuditing implements AuditorAware<String>
 {
     @Override
