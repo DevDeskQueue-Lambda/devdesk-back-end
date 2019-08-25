ecafa362cb9d4e6519d0a5c2bfb5c789c7ed4907
Donald Whitely <dswhitely1@gmail.com>
2019-08-25 10:50:22 -0400

    Started RoleRepository

---
+++ devdesk/src/main/java/com/digitalsolutionsbydon/devdesk/repositories/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.digitalsolutionsbydon.devdesk.repositories;
+
+import com.digitalsolutionsbydon.devdesk.models.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role, Long>
+{
+}
