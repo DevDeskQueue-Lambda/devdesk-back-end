2b9bc2e9b8bb2824ed39ddc9244314b9c8e8d0f2
Donald Whitely <dswhitely1@gmail.com>
2019-08-25 09:32:57 -0400

    ErrorDetail Model defined

---
+++ devdesk/src/main/java/com/digitalsolutionsbydon/devdesk/models/ErrorDetail.java
@@ -0,0 +1,79 @@
+package com.digitalsolutionsbydon.devdesk.models;
+
+import com.digitalsolutionsbydon.devdesk.exceptions.ValidationError;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ErrorDetail
+{
+    private String title;
+    private int status;
+    private String detail;
+    private String timestamp;
+    private String developerMessage;
+    private Map<String, List<ValidationError>> errors = new HashMap<>();
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public int getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(int status)
+    {
+        this.status = status;
+    }
+
+    public String getDetail()
+    {
+        return detail;
+    }
+
+    public void setDetail(String detail)
+    {
+        this.detail = detail;
+    }
+
+    public String getTimestamp()
+    {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp)
+    {
+        this.timestamp = new SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z").format(new Date(timestamp));
+    }
+
+    public String getDeveloperMessage()
+    {
+        return developerMessage;
+    }
+
+    public void setDeveloperMessage(String developerMessage)
+    {
+        this.developerMessage = developerMessage;
+    }
+
+    public Map<String, List<ValidationError>> getErrors()
+    {
+        return errors;
+    }
+
+    public void setErrors(Map<String, List<ValidationError>> errors)
+    {
+        this.errors = errors;
+    }
+}
