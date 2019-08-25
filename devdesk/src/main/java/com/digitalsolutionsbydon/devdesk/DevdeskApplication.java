22140c3a2de1ff7725898e0e8b21ca7544e67ab0
Donald Whitely <dswhitely1@gmail.com>
2019-08-25 09:11:34 -0400

    Initial Setup

---
+++ devdesk/src/main/java/com/digitalsolutionsbydon/devdesk/DevdeskApplication.java
@@ -0,0 +1,15 @@
+package com.digitalsolutionsbydon.devdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevdeskApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DevdeskApplication.class, args);
+    }
+
+}
